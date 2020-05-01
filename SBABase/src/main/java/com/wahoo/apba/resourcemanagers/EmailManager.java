80af06668aba39cb0cc574a2b0b7d198e356e2a0
Carl Phillips <cmp1166@gmail.com>
2020-05-01 16:27:43 -0400

    Update 2020 email list

--- SBABase/src/main/java/com/wahoo/apba/resourcemanagers/EmailManager.java
+++ SBABase/src/main/java/com/wahoo/apba/resourcemanagers/EmailManager.java
@@ -24,7 +24,7 @@ public class EmailManager
 
 	//FIXME  Remove hardcoded address in query
      private static String GET_PRIM_ADDRESS_SQL = "select * from emailaddresses where primaryaddress = 'Y' and memberid in " +
-         "(select t.memberid from teams as t join teamresults as tr on tr.teamid = t.id where tr.year=2019)";
+         "(select t.memberid from teams as t join teamresults as tr on tr.teamid = t.id where tr.year=2020)";
      private static String GET_EMAIL_FOR_OWNER = "select * from emailaddresses where memberid = ? and primaryaddress = 'Y'"; 
      
     /** Creates a new instance of EmailManager */
@@ -34,84 +34,66 @@ public class EmailManager
     
     public String getMemberAddresses ()
     {
-        StringBuffer wkAddresses = new StringBuffer(200);
+        final StringBuffer wkAddresses = new StringBuffer(200);
         Statement wkStatement = null;
         Connection wkConn = null;
         ResultSet wkResultSet = null;
-         
-        try
-        {
+
+        try {
             wkConn = DBUtil.getReadOnlyDBConnection();
             wkStatement = wkConn.createStatement();
-            
+
             wkResultSet = wkStatement.executeQuery(GET_PRIM_ADDRESS_SQL);
-            
-            while (wkResultSet.next())
-            {
+
+            while (wkResultSet.next()) {
                 if (wkAddresses.length() > 0)
                     wkAddresses.append(",");
                 wkAddresses.append(wkResultSet.getString("address"));
-            }            
+            }
             wkResultSet.close();
             wkStatement.close();
-        }
-        catch (Exception e)
-        {
+        } catch (final Exception e) {
             e.printStackTrace();
             Email.emailException(e);
-        }
-        finally
-        {
-            try
-            {
+        } finally {
+            try {
                 if (null != wkConn)
                     wkConn.close();
-            }
-            catch (SQLException wkException)
-            {
+            } catch (final SQLException wkException) {
                 wkException.printStackTrace();
                 Email.emailException(wkException);
             }
-        }  
-        
+        }
+
         return wkAddresses.toString();
     }
-    
-    public ArrayList<com.wahoo.apba.database.Email> getEmailAddresses (int inId)
-    {
-        ArrayList<com.wahoo.apba.database.Email> wkAddress = new ArrayList<com.wahoo.apba.database.Email>();
+
+    public ArrayList<com.wahoo.apba.database.Email> getEmailAddresses(final int inId) {
+        final ArrayList<com.wahoo.apba.database.Email> wkAddress = new ArrayList<com.wahoo.apba.database.Email>();
         PreparedStatement wkStatement = null;
         Connection wkConn = null;
         ResultSet wkResultSet = null;
-         
-        try
-        {
+
+        try {
             wkConn = DBUtil.getReadOnlyDBConnection();
             wkStatement = wkConn.prepareStatement(GET_EMAIL_FOR_OWNER);
             wkStatement.setInt(1, inId);
-            
+
             wkResultSet = wkStatement.executeQuery();
-            
-            while (wkResultSet.next())
-            {
+
+            while (wkResultSet.next()) {
                 wkAddress.add(new com.wahoo.apba.database.Email(wkResultSet));
-            }            
+            }
             wkResultSet.close();
             wkStatement.close();
-        }
-        catch (Exception e)
-        {
+        } catch (final Exception e) {
             e.printStackTrace();
             Email.emailException(e);
-        }
-        finally
-        {
-            try
-            {
+        } finally {
+            try {
                 if (null != wkConn)
                     wkConn.close();
-            }
-            catch (SQLException wkException)
+            } catch (final SQLException wkException)
             {
                 wkException.printStackTrace();
                 Email.emailException(wkException);

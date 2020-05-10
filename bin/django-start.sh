2471722dfb45d615a3fd9db129edda8aaf7b8504
Carl Phillips <cmp1166@gmail.com>
2020-05-10 11:10:38 -0400

    Changed to use prod settings

--- bin/django-start.sh
+++ bin/django-start.sh
@@ -1,3 +1,3 @@
-export DJANGO_SETTINGS_MODULE=nabl.settings.dev_settings
+export DJANGO_SETTINGS_MODULE=nabl.settings.prod_settings
 export PYTHONPATH=$PYTHONPATH:~/root
-python -B manage.py runfcgi host=127.0.0.1 port=8001 --settings=nabl.settings.dev_settings
+python -B manage.py runfcgi host=127.0.0.1 port=8001 --settings=nabl.settings.prod_settings
